7b1dee5b0513613bdc9c12b7de75444f9694ce81
Emre Dogan <emre.dogan@shortcut.no>
2023-05-03 12:39:48 +0200

    Initial Commit

---
+++ PhotoValidation/PhotoValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoValidationApp.swift
+//  PhotoValidation
+//
+//  Created by Emre Dogan on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
